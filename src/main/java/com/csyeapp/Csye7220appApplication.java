5b53d177a13c0f009aea41071487156187497df3
Aditya <adikelkar4@gmail.com>
2019-06-12 00:00:23 -0400

    Root commit

---
+++ src/main/java/com/csyeapp/Csye7220appApplication.java
@@ -0,0 +1,13 @@
+package com.csyeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csye7220appApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Csye7220appApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csyeapp/Csye7220appApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csyeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csye7220appApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
